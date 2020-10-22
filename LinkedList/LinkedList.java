42aae83b0a5b30ad9b2925bfc115f202ed253585
Achlesh Sharma <achleshsharma2010@gmail.com>
2020-10-22 17:39:40 +0530

    Update LinkedList.java

--- LinkedList/LinkedList.java
+++ LinkedList/LinkedList.java
@@ -184,4 +184,18 @@ public class LinkedList<T> {
 
         return retlist;
     }
+  int detectRemoveLoop(Node node) 
+    { 
+        Node slow = node, fast = node; 
+        while (slow != null && fast != null && fast.next != null) { 
+            slow = slow.next; 
+            fast = fast.next.next;
+            if (slow == fast) { 
+                removeLoop(slow, node); 
+                return 1; 
+            } 
+        } 
+        return 0; 
+    } 
 }
+
